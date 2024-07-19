a093c7f993fab9eb2f4bdcc086d968777ac84aa7
Sedat Budak <sbudak@cyh.com.tr>
2024-07-19 16:10:01 +0300

    feat: added detect language result model

---
+++ lib/core/models/response/detect_language_result_model/detect_language_result_model.dart
@@ -0,0 +1,22 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'detect_language_result_model.g.dart';
+
+@JsonSerializable()
+class DetectLanguageResultModel {
+  @JsonKey(name: 'detected_languages')
+  List<String>? detectedLanguages;
+  @JsonKey(name: 'original_characters')
+  int? originalCharacters;
+
+  DetectLanguageResultModel({
+    this.detectedLanguages,
+    this.originalCharacters,
+  });
+
+  factory DetectLanguageResultModel.fromJson(Map<String, dynamic> json) {
+    return _$DetectLanguageResultModelFromJson(json);
+  }
+
+  Map<String, dynamic> toJson() => _$DetectLanguageResultModelToJson(this);
+}
