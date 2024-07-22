b741cb8664c750458e9e02c4c67d4dae7f78b08a
Sedat Budak <sbudak@cyh.com.tr>
2024-07-22 16:14:43 +0300

    chore: added extra colors and space

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -5,7 +5,21 @@ class AppColors {
 
   static const Color text = Colors.black;
 
-  static const Color dividercolor = Color.fromARGB(255, 0, 55, 98);
+  static const Color dividerColor = Color.fromARGB(255, 0, 55, 98);
+
+  static const Color enabledBorder = Colors.black45;
+
+  static const Color focusedBorder = Colors.black87;
+
+  static const Color errorBorder = Colors.red;
+
+  static const Color focusedErrorBorder = Color.fromARGB(255, 200, 18, 5);
+
+  static const Color snackBarColor = Color.fromARGB(255, 0, 55, 98);
+
+  static const Color labelColor = Colors.white;
+
+  static const Color translateButtonColor = Color.fromARGB(255, 0, 55, 98);
 }
 
 class AppPadding {
@@ -17,9 +31,14 @@ sealed class AppSpacing {
   static const _kValue = 20;
 
   static SizedBox get smallVerticalSpace => const SizedBox(height: _kValue * 1);
+  static SizedBox get smallHorizontalSpace =>
+      const SizedBox(width: _kValue * 1);
+
   static SizedBox get mediumVerticalSpace =>
       const SizedBox(height: _kValue * 2);
   static SizedBox get largeVerticalSpace => const SizedBox(height: _kValue * 3);
+  static SizedBox get megaLargeVertical =>
+      const SizedBox(height: _kValue * 3 * 2);
 }
 
 //* context.pagePadding
