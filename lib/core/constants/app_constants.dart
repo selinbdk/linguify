aefafb200d7262c9b861dadfb55fb56cdd5d228f
Sedat Budak <sbudak@cyh.com.tr>
2024-07-23 12:08:21 +0300

    feat: added paths to api constants

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -3,9 +3,5 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class AppConstants {
   static String baseUrl = dotenv.env["base_url"] ?? "";
 
-  static String detectLanguagePath = dotenv.env["detect_language_path"] ?? "";
-
-  static String getListPath = dotenv.env["get_list_path"] ?? "";
-
-  static String translateTextPath = dotenv.env["translate_text_path"] ?? "";
+  static String apiToken = dotenv.env["api-token"] ?? "";
 }
